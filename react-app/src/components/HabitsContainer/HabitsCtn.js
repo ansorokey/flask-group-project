7976fdb46644b46321954b756e7f97e6d1cc5466
ansorokey <ansorokey@gmail.com>
2023-08-18 11:06:58 -0400

    added clickable habit filter buttons

--- react-app/src/components/HabitsContainer/HabitsCtn.js
+++ react-app/src/components/HabitsContainer/HabitsCtn.js
@@ -2,10 +2,12 @@
 import HabitItem from "./HabitItem.js"
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import "./habits.css"
 
 function HabitsCtn() {
     const sessionUser = useSelector((state) => state.session.user);
     const [habitTitle, setHabitTitle] = useState('');
+    const [habits, setHabits] = useState([]);
 
   function handleSubmit(e){
     //create the habit from form value
@@ -25,10 +27,15 @@ function HabitsCtn() {
     alert(habitTitle)
   }
 
+  function setSortActive(e){
+    document.querySelectorAll(".habit_filter_by").forEach( sp => sp.className = "habit_filter_by")
+    e.target.className += ' active'
+
+  }
 
   return <div>
     <span>
-      <h2>Habits</h2> <h2>Sort Buttons</h2>
+      <h2>Habits</h2> <span className="habit_filter_by active" onClick={setSortActive}>All</span> <span className="habit_filter_by" onClick={setSortActive}>Weak</span> <span className="habit_filter_by" onClick={setSortActive}>Strong</span>
     </span>
 
     <form onSubmit={handleSubmit}>
