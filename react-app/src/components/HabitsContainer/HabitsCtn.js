40fac778e36db8b320fa6e16ebdfb4bcd4da9864
ansorokey <ansorokey@gmail.com>
2023-08-19 13:03:27 -0400

    successful habit sorting

--- react-app/src/components/HabitsContainer/HabitsCtn.js
+++ react-app/src/components/HabitsContainer/HabitsCtn.js
@@ -1,7 +1,7 @@
 // need to import a reactState
 import HabitList from "./HabitList.js"
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./habits.css"
 
 function HabitsCtn() {
@@ -9,40 +9,53 @@ function HabitsCtn() {
     const [habitTitle, setHabitTitle] = useState('');
     const [habits, setHabits] = useState([]);
     const [filteredHabits, setFilteredHabits] = useState(habits);
+    const [filterBy, setFilterBy] = useState('All');
+
+    useEffect(() => {
+      setFilteredHabits(() => {
+        switch (filterBy) {
+          case 'All':
+            return habits;
+          case 'Weak':
+            return habits.filter(h => h.strength == 'Weak');
+          case 'Strong':
+            return habits.filter(h => h.strength == 'Strong');
+          default:
+            return habits;
+        }
+      });
+    }, [habits, filterBy]);
 
   function handleSubmit(e){
-    //create the habit from form value
-    //post habit to db
-    //add habit to store
-    //auto reload store
-    //havit should appear at top, default order by is created at?
-    //updating should retain order
-    //reset form value
-    // Dont allow the submission to go through yet
     e.preventDefault();
+
     const newHabit = {
         title: habitTitle,
-        user_id: sessionUser.id
+        user_id: sessionUser.id,
+        strength: 'Weak'
     }
 
     setHabits(prev => {
         const newVal = [newHabit, ...prev]
-        console.log(newVal)
         return newVal;
     });
-    // setFilteredHabits(habits)
+
     setHabitTitle("");
   }
 
   function setSortActive(e){
     document.querySelectorAll(".habit_filter_by").forEach( sp => sp.className = "habit_filter_by")
     e.target.className += ' active'
-
+    setFilterBy(e.target.id)
+    console.log(e.target.id)
   }
 
   return <div>
     <span>
-      <h2>Habits</h2> <span className="habit_filter_by active" onClick={setSortActive}>All</span> <span className="habit_filter_by" onClick={setSortActive}>Weak</span> <span className="habit_filter_by" onClick={setSortActive}>Strong</span>
+      <h2>Habits</h2>
+      <span id="All" className="habit_filter_by active" onClick={setSortActive}>All</span>
+      <span id="Weak" className="habit_filter_by" onClick={setSortActive}>Weak</span>
+      <span id="Strong" className="habit_filter_by" onClick={setSortActive}>Strong</span>
     </span>
 
     <form onSubmit={handleSubmit}>
@@ -54,7 +67,7 @@ function HabitsCtn() {
     </form>
 
     <h2>habits_list-ctn</h2>
-    <HabitList habits={habits} />
+    <HabitList habits={filteredHabits} />
   </div>
 }
 export default HabitsCtn;
