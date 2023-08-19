fd258752771221260ac9b2017bd7175f9f325df0
ansorokey <ansorokey@gmail.com>
2023-08-19 14:25:01 -0400

    general form

---
+++ react-app/src/components/EditHabitForm/EditHabitForm.js
@@ -0,0 +1,56 @@
+function EditHabitForm(){
+    return <div>
+
+        <form className="edit-habit-form">
+            <div className="edit-form-top">
+                <label>Title</label>
+                <input type="text"
+                    value="title goes here"
+                />
+
+                <label>Notes</label>
+                <textarea
+                    placeholder="Add Notes"
+                ></textarea>
+            </div>
+
+            <div classname='edit-form-bottom'>
+                <div>
+                    <div>
+                        <button>+</button>
+                        <div>Positive</div>
+                    </div>
+                    <div>
+                        <button>-</button>
+                        <div>Negative</div>
+                    </div>
+                </div>
+
+                <label>Difficulty</label>
+                <select>
+                    <option value="1">Trivial</option>
+                    <option value="2">Easy</option>
+                    <option value="3">Medium</option>
+                    <option value="4">Hard</option>
+                </select>
+
+                <label>Tags</label>
+                <select>
+                    <option>Need</option>
+                    <option>To</option>
+                    <option>Query</option>
+                    <option>Tags</option>
+                </select>
+
+                <label>Reset Counter</label>
+                <select>
+                    <option>Daily</option>
+                    <option>Weekly</option>
+                    <option>Monthly</option>
+                </select>
+            </div>
+
+        </form>
+        <button>Delete This Habit</button>
+    </div>
+}
