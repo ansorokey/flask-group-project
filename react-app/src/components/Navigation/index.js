efa518b882ec80ca4b16f36d11b5ac27cf99311e
ansorokey <ansorokey@gmail.com>
2023-08-22 13:12:42 -0400

    navigation bar styling

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -9,9 +9,18 @@ function Navigation({ isLoaded }){
 
 	return (
 		<div className='nav-bar'>
-			<div>Menu</div>
+			<div hidden="true">Menu</div>
 
+			<div className='nav-links-ctn'>
 			<NavLink exact to="/" className="nav-home">LevelUp!</NavLink>
+				<NavLink className="nav-links" exact to="/">Tasks</NavLink>
+				<NavLink className="nav-links" exact to="/inventory">Inventory</NavLink>
+				<NavLink className="nav-links" exact to="/shops">Shops</NavLink>
+				<NavLink className="nav-links" exact to="/party">Party</NavLink>
+				<NavLink className="nav-links" exact to="/groups">Group</NavLink>
+				<NavLink className="nav-links" exact to="/challenges">Challenges</NavLink>
+				<NavLink className="nav-links" exact to="/help">Help</NavLink>
+			</div>
 
 			{isLoaded && <ProfileButton user={sessionUser} />}
 		</div>
