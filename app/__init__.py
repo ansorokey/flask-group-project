baf1390afe8b4303947fbb7cc3425b180a995a35
ansorokey <ansorokey@gmail.com>
2023-08-28 20:07:52 -0400

    missed some merges

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,12 @@ from flask import Flask, render_template, request, session, redirect, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
-<<<<<<< HEAD
 from flask_login import LoginManager, current_user
-from .models import db, User
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
-from .api.habit_routes import habit_routes
-=======
-from flask_login import LoginManager
 from .models import db, User, Daily
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.habit_routes import habit_routes
 from .api.dailies_routes import daily_bp
->>>>>>> dailies
 from .seeds import seed_commands
 from .config import Config
 
@@ -38,12 +31,8 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
-<<<<<<< HEAD
 app.register_blueprint(habit_routes, url_prefix='/api/habits')
-=======
 app.register_blueprint(daily_bp, url_prefix='/api/dailies')
-
->>>>>>> dailies
 db.init_app(app)
 Migrate(app, db)
 
