08de26de2a05335ee685a2c8d54b774394172022
ansorokey <ansorokey@gmail.com>
2023-09-07 10:27:57 -0400

    more landing page styling

--- react-app/src/components/SignupFormPage/index.js
+++ react-app/src/components/SignupFormPage/index.js
@@ -13,7 +13,7 @@ function SignupFormPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState({});
 
   if (sessionUser) return <Redirect to="/" />;
 
@@ -22,7 +22,12 @@ function SignupFormPage() {
     if (password === confirmPassword) {
         const data = await dispatch(signUp(username, email, password, firstName, lastName));
         if (data) {
-          setErrors(data)
+          const errs = {}
+          data.forEach(e => {
+            const keyMessage = e.split(':');
+            errs[keyMessage[0].trim()] = keyMessage[1].trim();
+          });
+          setErrors(errs);
         }
     } else {
         setErrors(['Confirm Password field must be the same as the Password field']);
@@ -31,66 +36,67 @@ function SignupFormPage() {
 
   return (
     <div className="signup-page">
-      <h1>Sign Up</h1>
+      <h2>Sign Up For Free</h2>
+      <p>Username must be 1 to 20 characters, containing only letters a to z, numbers 0 to 9, hyphens, or underscores, and cannot include any inappropriate terms.</p>
       <form onSubmit={handleSubmit} className="signup-page-form">
-        <ul>
-          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-				<label>
-					First name
-					<input
-						type="text"
-						value={firstName}
-						onChange={(e) => setFirstName(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Last name
-					<input
-						type="text"
-						value={lastName}
-						onChange={(e) => setLastName(e.target.value)}
-						required
-					/>
-				</label>
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Sign Up</button>
+
+        <input
+          placeholder="Email"
+          className="signup-input"
+          type="text"
+          value={email}
+          onChange={(e) => {setEmail(e.target.value); setErrors(prev => {delete prev.email; return {...prev}});}}
+          required
+        />
+        {errors.email}
+
+        <input
+          placeholder="First Name"
+          className="signup-input"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+
+        <input
+          placeholder="Last Name"
+          className="signup-input"
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+
+        <input
+          placeholder="Username"
+          className="signup-input"
+          type="text"
+          value={username}
+          onChange={(e) => {setUsername(e.target.value); setErrors(prev => {delete prev.username; return {...prev}});}}
+          required
+        />
+        {errors.username}
+
+        <input
+          placeholder="Password"
+          className="signup-input"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <input
+          placeholder="Confirm Password"
+          className="signup-input"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+
+        <button className="signup-button" type="submit">Sign Up</button>
       </form>
     </div>
   );
