4700f5bbb879ed5425f87cf61bcdacf941129208
Alex Florea <alexflorea@Alexs-MacBook-Pro.local>
2023-09-09 17:56:05 -0400

    Final?

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,12 +1,19 @@
 import "./Search.css";
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { createTodoForUser } from '../../store/todos'; 
 import { useModal } from "../../context/Modal";
 import EditHabitForm from "../EditHabitForm/EditHabitForm";
-
+import TodoForm from "../ToDoCont/todoform"; 
 
 function Search() {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.session.user);
+    const userId = user ? user.id : null;
+
     const [tagsOpen, setTagsOpen] = useState(false);
     const [tasksOpen, setTasksOpen] = useState(false);
+    const [newTodo, setNewTodo] = useState('');
     const { setModalContent } = useModal();
 
     const defaultHabit = {
@@ -18,9 +25,8 @@ function Search() {
         neg: true,
         posCount: 0,
         negCount: 0
-    }
+    };
 
-    // 100% credit to Christine on this
     useEffect(() => {
         const closeMenuOutsideClick = (e) => {
           if (tagsOpen || tasksOpen) {
@@ -34,65 +40,88 @@ function Search() {
         };
       }, [tagsOpen, tasksOpen]);
 
+    const openAddQuestModal = () => {
+        setModalContent(
+            <TodoForm
+                initialTitle={newTodo}
+                onSubmit={(data) => {
+                    handleAddTodo(data);
+                    setModalContent(null);
+                }}
+                onCancel={() => setModalContent(null)}
+            />
+        );
+    };
+
+    const handleAddTodo = async (data) => {
+        if(userId) {
+            try {
+                await dispatch(createTodoForUser(userId, data));
+                setNewTodo('');
+            } catch (error) {
+                console.error("Error adding new todo:", error.message);
+            }
+        } else {
+            console.error("User ID not found");
+        }
+    };
+
     const taskOptions = (
-        <div
-            className="task-options"
-        >
+        <div className="task-options">
             <div className="createMenuOption" onClick={() => setModalContent(<EditHabitForm habit={defaultHabit} edit={false}/>)}>
                 <i className="fa-solid fa-cubes-stacked"></i>
                 &nbsp;&nbsp;Habit
             </div>
 
             <div className="createMenuOption">
-                <span><i class="fa-solid fa-calendar-days"></i></span>
+                <span><i className="fa-solid fa-calendar-days"></i></span>
                 &nbsp;&nbsp;Daily
             </div>
-            <div>To Do</div>
+           
+            <div onClick={openAddQuestModal}>To Do</div>
             <div>Reward</div>
         </div>
     );
 
-    return (<div className="search-ctn">
-        <form>
-            <input
-                className="search-input"
-                type="text"
-                placeholder="Search"
-            />
-        </form>
+    return (
+        <div className="search-ctn">
+            <form>
+                <input
+                    className="search-input"
+                    type="text"
+                    placeholder="Search"
+                />
+            </form>
 
-        <div
-            className="search-tags"
-            onClick={() => setTagsOpen(!tagsOpen)}
-        >
-            <i className="fa-solid fa-sliders"></i>
-            <span
-                className="search-tags-text"
+            <div
+                className="search-tags"
+                onClick={() => setTagsOpen(!tagsOpen)}
             >
-                Tags
-            </span>
-            {tagsOpen ? <i className="fa-solid fa-caret-up"></i> : <i className="fa-solid fa-caret-down"></i>}
-            {tagsOpen &&
-                <div className="tags-menu">
-                    <p>Feature coming <s>never</s> soon!</p>
-                    <img src="https://res.cloudinary.com/dzntryr5a/image/upload/v1694130506/Rowlet-Pokemon-Transparent-PNG_xr1qfl.png"/>
-                </div>
-            }
-        </div>
+                <i className="fa-solid fa-sliders"></i>
+                <span className="search-tags-text">
+                    Tags
+                </span>
+                {tagsOpen ? <i className="fa-solid fa-caret-up"></i> : <i className="fa-solid fa-caret-down"></i>}
+                {tagsOpen &&
+                    <div className="tags-menu">
+                        <p>Feature coming <s>never</s> soon!</p>
+                        <img src="https://res.cloudinary.com/dzntryr5a/image/upload/v1694130506/Rowlet-Pokemon-Transparent-PNG_xr1qfl.png"/>
+                    </div>
+                }
+            </div>
 
-        <div
-            className="search-add-task"
-            onClick={() => setTasksOpen(!tasksOpen)}
-        >
-            <i className="fa-solid fa-plus"></i>
-            <span
-                className="add-task-text"
+            <div
+                className="search-add-task"
+                onClick={() => setTasksOpen(!tasksOpen)}
             >
-                    Add Task
-            </span>
-            {tasksOpen && taskOptions}
+                <i className="fa-solid fa-plus"></i>
+                <span className="add-task-text">
+                        Add Task
+                </span>
+                {tasksOpen && taskOptions}
+            </div>
         </div>
-    </div>);
+    );
 }
 
 export default Search;
