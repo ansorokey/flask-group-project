311dab16e49725e5c761ef4d7aa8844c037db45e
christine parsons <clong9712@gmail.com>
2023-10-14 13:33:23 -0400

    notes added into editDailyForm

--- react-app/src/components/DailyCont/EditDailyForm.js
+++ react-app/src/components/DailyCont/EditDailyForm.js
@@ -22,11 +22,11 @@ function EditDailyForm({daily}) {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const issues = {}
 
+    // Build a dummy error object then set it when all the conditions are checked
+    const issues = {}
     if(!title.length) {
       issues.title = "Title is required"
-
     }
     if(title && (title.length < 3 || title.length > 50)){
       issues.title = "Title must be between 3 and 50 characters"
@@ -38,9 +38,10 @@ function EditDailyForm({daily}) {
       setErrors({})
     }
 
+    // build a daily object with all the information gathered from the form. +streak ensures streak is an integer
     const newDaily = { title, description, strength, repeats_frame, repeats_frequency, streak: +streak };
 
-
+    // check if there are errors before dispatching
     if(!Object.values(issues).length){
       await dispatch(updateDaily(daily.id, newDaily));
       closeModal();
@@ -49,6 +50,7 @@ function EditDailyForm({daily}) {
   }
 
   const getTimeframe = () => {
+    // converts a number into a string used to display length of time chosen
     if (repeats_frame === "1") return "Days"
     else if (repeats_frame === "7") return "Weeks"
     else if (repeats_frame === "30") return "Months"
@@ -62,10 +64,13 @@ function EditDailyForm({daily}) {
 
       <div className="daily-title-and-btns" >
 
+
+        {/* Title */}
         <div>
           Edit Daily
         </div>
 
+        {/* Buttons */}
         <div>
           <button className="daily-edit cancel" onClick={closeModal}>
             Cancel
@@ -76,132 +81,192 @@ function EditDailyForm({daily}) {
               Save
           </button>
         </div>
+
       </div>
+
 
     <form className="daily-habit-form" onSubmit={handleSubmit}>
 
+
+      {/* this div covers the top section to give it its styling */}
       <div className="daily-edit-form-top">
 
-      <div className="daily-edit-input-ctn">
-            <label>Title</label>
-            <input
-              type='text'
-              value={title}
-              placeholder="Add a title"
-              className="daily-edit-form-top-input"
-              onChange={(e)=> setTitle(e.target.value)}
-            />
-            <div className="errors">{errors?.title}</div>
-      </div>
+        <div className="daily-edit-input-ctn">
+        {/* This div is for the title input */}
+              <label>
+                Title
+              </label>
 
-          <div className="daily-edit-input-ctn">
-            <label>Notes</label>
+              <input
+                type='text'
+                value={title}
+                placeholder="Add a title"
+                className="daily-edit-form-top-input"
+                onChange={(e)=> setTitle(e.target.value)}
+              />
 
-            <textarea
-              placeholder="Add notes"
-              className="daily-edit-form-top-input"
-              value={description}
-              onChange={(e)=> setDescription(e.target.value)}
-            />
+              <div className="errors">
+                {errors?.title}
+              </div>
+        </div>
+
+        <div className="daily-edit-input-ctn">
+          {/* This div is for the description input */}
+          <label>
+            Notes
+          </label>
+
+          <textarea
+            placeholder="Add notes"
+            className="daily-edit-form-top-input"
+            value={description}
+            onChange={(e)=> setDescription(e.target.value)}
+          />
+
+          <div className="errors">
+            {errors?.description}
           </div>
+
+        </div>
+
+      {/* End of top of form */}
       </div>
-      <div className="errors">{errors?.description}</div>
+
 
 
       <div className="daily-edit-form-bottom">
-            {/* This is where checklist input will go in the future */}
+        {/* This is where checklist input will go in the future */}
 
-          <div className="edit-daily-select-ctn">
-              <label id="gap">Difficulty </label>
+        <div className="edit-daily-select-ctn">
+        {/* Difficulty selector div */}
+          <label id="gap">
+            Difficulty
+          </label>
 
-              <select value={strength} onChange={(e) => setStrength(e.target.value)}>
-                <option value="Trivial">Trivial</option>
-                <option value="Easy">Easy</option>
-                <option value="Medium">Medium</option>
-                <option value="Hard">Hard</option>
-              </select>
+          <select value={strength} onChange={(e) => setStrength(e.target.value)}>
+            <option value="Trivial">Trivial</option>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
+          </select>
 
-              <div className="errors">{errors?.difficulty}</div>
+          <div className="errors">
+            {errors?.difficulty}
+          </div>
+        </div>
+
+        {/* This is where Start date will go when it is implimented */}
+
+        <div className="edit-daily-select-ctn">
+          {/* This is the repeats time frame input div */}
+          <label>
+            Repeats
+          </label>
+
+          <select value={repeats_frame} onChange={(e) => setRepeats_frame(e.target.value)}>
+            <option value="1">Daily</option>
+            <option value="7">Weekly</option>
+            <option value="30">Monthly</option>
+            <option value="365">Yearly</option>
+          </select>
+
+          <div className="errors">
+            {errors?.repeats_frame}
           </div>
 
+        </div>
 
+        <div className="edit-daily-select-ctn">
+          {/* This div is the number input for how many of time frame should pass before task becomes due again */}
+          <label>
+            Repeat Every
+          </label>
 
-          {/* This is where Start date will go when it is implimented */}
-          <div className="edit-daily-select-ctn">
-            <label>Repeats</label>
-            <select value={repeats_frame} onChange={(e) => setRepeats_frame(e.target.value)}>
-              <option value="1">Daily</option>
-              <option value="7">Weekly</option>
-              <option value="30">Monthly</option>
-              <option value="365">Yearly</option>
-            </select>
-            <div className="errors">{errors?.repeats_frame}</div>
+          <div>
+            <input type='number'  min="1" value={repeats_frequency} onChange={(e)=> setRepeats_frequency(e.target.value)} />
+            {/* This is where the selected time frame is displayed (days, weeks, months, years) after the input field. &ensp; adds some spacing between the two */}
+            &ensp;
+            <span className="timeFrame">
+              {getTimeframe()}
+            </span>
           </div>
 
-          <div className="edit-daily-select-ctn">
-            <label>Repeat Every</label>
-            <div>
-              <input type='number'  min="1" value={repeats_frequency} onChange={(e)=> setRepeats_frequency(e.target.value)} />    &ensp; <span className="timeFrame">{getTimeframe()}</span>
-            </div>
-
-            <div className="errors">{errors?.repeats_frequency}</div>
+          <div className="errors">
+            {errors?.repeats_frequency}
           </div>
 
+        </div>
 
-          {/* This is where the tags input will go when that feature is implimented  */}
+
+        {/* This is where the tags input will go when that feature is implimented  */}
 
 
 
 
       <div className="advanced-options-ctn">
+        {/* This is the bottom part where an advanced options section can be expanded */}
 
         <div className="advancedTitle" onClick={() => setAdvanced(!advanced)}>
+          {/* This section toggles the advanced section from being hidden/ to being shown */}
           <div className="advanced-options-chevron">
-              <label>Advanced Settings</label>
-
-              { advanced ? <i className="fa-solid fa-caret-up"></i> : <i className="fa-solid fa-caret-down"></i> }
-
-            </div>
-        </div>
-
-        { advanced &&
-        <div className="advanced-options-menu" >
-
-          <label id="streakLabel">Adjust Streak</label>
-
-          <div className="advanced-counters">
-            <div className="streakIcon">
-              <i className="fa-solid fa-forward icon-forward"></i>
-
-              <input type="number" value={streak} min="0" onChange={(e)=>{setStreak(e.target.value)}}/></div>
-
+            <label>
+              Advanced Settings
+            </label>
+            {/* conditional logic to decide which icon to display: an arrow facing up if the section is open or facing down if it is closed */}
+            { advanced ? <i className="fa-solid fa-caret-up"></i> : <i className="fa-solid fa-caret-down"></i> }
           </div>
         </div>
 
+
+        {/* This is the section that will be hidden or displayed based on status of advanced useState variable */}
+        { advanced && <div className="advanced-options-menu" >
+
+          <label id="streakLabel">
+            Adjust Streak
+          </label>
+
+          <div className="advanced-counters">
+            <div className="streakIcon">
+
+              <i className="fa-solid fa-forward icon-forward"></i>
+              <input type="number" value={streak} min="0" onChange={(e)=>{setStreak(e.target.value)}}/>
+
+            </div>
+
+          </div>
+
+        </div>
         }
-        <div className="errors">{errors?.streak}</div>
+        <div className="errors">
+          {errors?.streak}
+        </div>
 
       </div>
-      </div>
+    </div>
   </form>
 
-      <div className="edit-daily-del">
-        <button className="edit-daily-del-btn"
-          onClick={() => {
-            const res = window.confirm("Are you sure you want to delete this daily?")
-            if(res) {
-              dispatch(removeDaily(daily.id));
-              closeModal();
-            }
-          }}
-        >
-          <i className="fa-solid fa-trash-can"></i>
-            &ensp;Delete this Daily
-        </button>
-      </div>
+{/* This is the bottom section that contains the delete part */}
+  <div className="edit-daily-del">
+    <button className="edit-daily-del-btn"
+      onClick={() => {
 
-    </div>
-  );
-}
+        // This pops up an alert when the button is clicked
+        const res = window.confirm("Are you sure you want to delete this daily?")
+
+        // If the user choses yes then res will be true so we dispatch the action and close the edit modal
+        if(res) {
+          dispatch(removeDaily(daily.id));
+          closeModal();
+    } }}>
+
+      {/* This is the trashcan Icon */}
+      <i className="fa-solid fa-trash-can"></i>
+      {/* This is the text with &ensp; to add some spacing between the icon and the text */}
+      &ensp;Delete this Daily
+
+    </button>
+  </div>
+
+</div>)}
 
 export default EditDailyForm;
--- react-app/src/components/DailyCont/index.js
+++ react-app/src/components/DailyCont/index.js
@@ -18,32 +18,28 @@ function DailyCont () {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        // Handle cases with improper title input and display proper error
+
         let err = {}
         if (!title) return
         if (title && (title.length > 50 || title.length < 3)) {
             err.title = "Title must be be between 3 and 50 characters"
         }
         if (Object.values(err).length){
+            // must use a dummy object to create all error key/value pairs THEN set them all at once in the real error object or there will be issues
             setError(err)
             return
         }else{
+            // this is so the errors go away when a new input is submitted that is valid
             setError({})
         }
-
-
-
         const newDaily = {title: title}
-
         await dispatch(createDaily(newDaily))
         dispatch(loadAllDailies())
         setTitle('')
-
     }
 
-
-
     const dailies = useSelector((state) => state.daily)
-
 
 
     return <div className="daily_cont">
@@ -53,7 +49,7 @@ function DailyCont () {
             {/* display a little purple circle with the number of how many dailies are due */}
         </div>
         <div className="titleRight" >
-
+            {/* These are my top tabs that flip between the data sets to display */}
             <input type="radio" value="due" id="due" name="daily" checked={cat === 'due'} onChange={(e)=>{setCat(e.target.value)}} />
             <label htmlFor="due">Due</label>
 
@@ -67,24 +63,30 @@ function DailyCont () {
     </div>
 
     <div className="greyBox">
-
+        
+        {/* This is the add title part to create a new habit */}
         <form className="habit-quick-add" onSubmit={handleSubmit} >
             <input
                 type="text"
                 placeholder="Add a Daily"
                 value={title}
-
                 onChange={e => setTitle(e.currentTarget.value)}
             />
             {error.title}
+            {/* When a form doesnt have a submit button the default functionality is to submit on enter*/}
         </form>
+
+
         <div>
+            {/* Here is where we loop through every indv daily to display a card for each one */}
             {Object.values(dailies[cat]).map((daily) => (
                 <DisplayDailyItems key={daily.id} daily={daily} />
             ))}
         </div>
-        <div className="ExplainBox">
 
+
+        <div className="ExplainBox">
+                {/* This is the footer at the bottom that gives a little info on how to use this catagory */}
             <div><i className="fa-solid fa-calendar-days"></i></div>
             <div>These are your Dailies</div>
             <div>Dailies repeat on a regular basis. Choose the schedule that works best for you!</div>
